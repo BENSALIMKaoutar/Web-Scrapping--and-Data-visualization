31125d608e248be10fa24a54b175e9091966f84b
BENSALIMKaoutar <118397882+BENSALIMKaoutar@users.noreply.github.com>
2023-12-01 05:34:12 -1100

    Update Web_Scrapping.py
    commented code

--- web_Scrapping&Data_visualization/Web_Scrapping.py
+++ web_Scrapping&Data_visualization/Web_Scrapping.py
@@ -5,25 +5,25 @@ import requests
 
 L=[0]*7
 site='https://www.babelio.com'
-url1='https://www.babelio.com/decouvrir.php'
+url1='https://www.babelio.com/decouvrir.php'               # page contenant des livres rassemblés par catégorie
 html1=requests.get(url1).content
 sel1=Selector(text=html1)
 locale.setlocale(locale.LC_ALL, 'nl_NL')
-a='//*[@id="page_corps"]/div/div[4]/div[4]/div[*]/a/@href'
+a='//*[@id="page_corps"]/div/div[4]/div[4]/div[*]/a/@href' # liens vers chaque catégorie
 urrl=sel1.xpath(a).extract()
 for i in range(4,len(urrl)-1):
     html2=requests.get(site+urrl[i]).content
     sel2=Selector(text=html2)
     u=sel2.xpath('//*[@id="page_corps"]/div[1]/div[5]/a/@href').extract()
-    url3=site+u[0]
+    url3=site+u[0]                                         # lien vers les livres populaires d'une catégorie déterminée
     html3=requests.get(url3).content
     sel3=Selector(text=html3)
     dd='//*[@id="page"]/div[2]/div[*]/a[1]/h2/text()'
-    Title=sel3.xpath(dd).extract()
+    Title=sel3.xpath(dd).extract()                         # Extraire les titres des livres
     f='//*[@id="page"]/div[2]/div[*]/a[2]/h3/text()'
-    Author=sel3.xpath(f).extract()
+    Author=sel3.xpath(f).extract()                         # Extraire les noms des auteurs
     refpath='//*[@id="page"]/div[2]/div[*]/a[1]/@href'
-    refRating=sel3.xpath(refpath).extract()
+    refRating=sel3.xpath(refpath).extract()                # Les liens vers la description d'un livre afin d'extraire son Rating at la note
     Rating=[]
     Note=[]
     cpt=0
@@ -32,9 +32,9 @@ for i in range(4,len(urrl)-1):
         html4=requests.get(url4).content
         sel4=Selector(text=html4)
         q='//*[@id="histogramme"]/div[1]/div[1]/text()'
-        q1=sel4.xpath(q).extract()
+        q1=sel4.xpath(q).extract()                   
         q2=sel4.xpath('//*[@id="page_corps"]/div/div[3]/div[2]/div[1]/div[2]/span[3]/a/span/text()').extract()
-        if len(q1)==0 or len(q2)==0 :
+        if len(q1)==0 or len(q2)==0 :                      # Nettoyer les données
             Title.pop(j-cpt)
             Author.pop(j-cpt)
             cpt+=1
@@ -53,7 +53,7 @@ for i in range(4,len(urrl)-1):
 
     L[i-4]={'Title':Title,'Author':Author,'Rating':Rating,'Note':Note}
 
-thriller=pd.DataFrame(L[0])
+thriller=pd.DataFrame(L[0])  
 bdessiné=pd.DataFrame(L[1])
 manga=pd.DataFrame(L[2])
 jeunesse=pd.DataFrame(L[3])
@@ -61,7 +61,7 @@ jeuneadlt=pd.DataFrame(L[4])
 imaginaire=pd.DataFrame(L[5])
 amour=pd.DataFrame(L[6])
 
-#conctaner tous les datframes en un seul
+#ajouter une colnne "genre" et concatener tous les dataframes en une seule
 import matplotlib.pyplot as plt
 N=[thriller,bdessiné,jeuneadlt,imaginaire,amour]
 M=[['thriller'],['bdessiné'],['jeuneadlt'],['imaginaire'],['amour']]
@@ -70,16 +70,7 @@ for frame in N:
     frame['genre']=M[i]*len(frame)
     i=i+1
 thriller
-books=pd.concat(N)
-#conctaner tous les datframes en un seul
-import matplotlib.pyplot as plt
-N=[thriller,bdessiné,jeuneadlt,imaginaire,amour]
-M=[['thriller'],['bdessiné'],['jeuneadlt'],['imaginaire'],['amour']]
-i=0
-for frame in N:
-    frame['genre']=M[i]*len(frame)
-    i=i+1
-thriller
+
 books=pd.concat(N)
 books
 #tracer la distribution du rating pour chaque categorie
